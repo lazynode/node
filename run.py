bde50d686b8fb2421282e3bdef3e3b87703e1c6f
vang1ong7ang <vang1ong7ang@outlook.com>
2021-09-25 06:34:04 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import time
 
 
 with subprocess.Popen(["./.node/neo-cli/bin/Release/net5.0/neo-cli"], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as proc:
-    time.sleep(60)
+    time.sleep(300)
     proc.communicate(b'exit\n', timeout=16)
